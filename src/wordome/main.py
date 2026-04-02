5b68c4e7ff448e708198c52f0be2f4f5c923d062
null pototo <null@pototo>
2026-04-02 13:20:34 -0400

    * added resources/urls.txt to source urls from txt file * updated environment config files (environment.yaml, pyproject.toml)

--- src/wordome/main.py
+++ src/wordome/main.py
@@ -1,33 +1,31 @@
-from typing import List
+from typing import Dict, List
 
 from wordome.infrastructure import WebFetcherManager
 from wordome.domain import WordStatsExtractor, WordStats
 
-# A simple script to verify
+from importlib import resources
+import wordome.resources as urls_source
 
-# url = "http://en.wikipedia.org/wiki/Web_scraping"
+# A script to verify basic functionality
 
-# url = "https://www.google.com"
-
-# url = "http://www.wayfair.com"
-url = "https://www.wayfair.com/kitchen-tabletop/pdp/vitamix-stainless-steel-container-vtm10031.html"
-
-# url = "http://www.amazon.com/Fendi-Pre-loved-Patent-Leather-Baguette/dp/B0GT6VHKBS/"
-# url = 'http://www.homedepot.com/p/6ft-x-8ft-Pine-Pressure-Treated-Privacy-Dog-Ear-Flat-Wood-Fence-Panel-158083/203733689'
-
-
-# initalize relevant components
+# Component to fetch HTML content
 fetcher_manager = WebFetcherManager()
+
+# Component to operate/process the HTML content (business logic)
 extractor = WordStatsExtractor()
 
-# invoke get request; should retreive the raw HTML (from GET response)
-html_raw = fetcher_manager.fetch(url)
+# URLs (temporarily sourced from local file)
+resource_path = resources.files(urls_source).joinpath("urls.txt")
+urls = resource_path.read_text(encoding="utf-8").splitlines()
+print(f"URLS: {urls}")
 
-if html_raw:
-    print(f"CONTENT (len): {len(html_raw)}")
-    word_stats: List[WordStats] = extractor.process(html_raw)
+# Trigger GET requests; fetches raw HTML content (per url)
+html_results: List[str] = fetcher_manager.fetch_all(urls)
+content_map: Dict[str, str] = {url: html for url, html in zip(urls, html_results)}
+
+# Run content through dummy process to demo processings (WordStatsExtractor)
+for url, html in content_map.items():
+    word_stats: List[WordStats] = extractor.process(html)
+    print(f"URL: {url}")
     for item in word_stats:
         print(f"\tword={item.word}, count={item.count}, freq={item.frequency}")
-
-# header test (DEBUG ONLY)
-# fetcher_manager.fetch_header_test()
