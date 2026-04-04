1f1f3081da7d803faf8d909bd4193c955716a863
Joe Pototo <workworkjoseph@gmail.com>
2026-04-03 22:09:08 -0400

    wip

--- src/wordome/domain/__init__.py
+++ src/wordome/domain/__init__.py
@@ -1,3 +1,9 @@
+from .review_section_detector import ReviewDetectionResult, ReviewSectionDetector
 from .word_stats_extractor import WordStats, WordStatsExtractor
 
-__all__ = ["WordStats", "WordStatsExtractor"]
+__all__ = [
+    "ReviewDetectionResult",
+    "ReviewSectionDetector",
+    "WordStats",
+    "WordStatsExtractor",
+]
